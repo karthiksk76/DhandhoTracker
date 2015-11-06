515a463112adb1f9c4a23380103acf72575a9c87
Karthik Kalyanaraman <karthiksk@msn.com>
2015-11-05 20:56:12 -0800

    Testing

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace DhandhoTracker
         [STAThread]
         static void Main()
         {
+            //
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DhandoInvestorPortfolio());            
